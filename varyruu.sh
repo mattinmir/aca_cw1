1259630ad61b10fa6231f86d2c6cc47ffe83ddf9
Mattin Mir-Tahmasebi <mm5213@matrix43.doc.ic.ac.uk>
2017-02-14 19:58:23 +0000

    data

--- varyruu.sh
+++ varyruu.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-echo  "Config order: -bpred -ruu:size -fetch:ifqsize "
+echo -e "bpred\truu:size\tifqsize\tIPC\tEnergy"
 
 for bpred in nottaken taken perfect bimod 2lev comb; do 
 	for ruu in 2 4 8 16 32 64 128 256; do
 		for fetch in 1 2 4 8 16; do
 								export SSFLAGS="-ruu:size $ruu -fetch:ifqsize $fetch -bpred $bpred"
 								
-								config=$bpred' '$ruu' '$fetch' '
+								config=$bpred'\t'$ruu'\t'$fetch
 								path=$bpred'_'$ruu'_'$fetch'_'
 								
 								./run-wattch  &>  /tmp/stats_$path
-								echo -n -e "\n$config\t\tIPC\t"
-								cat /tmp/stats_$path | grep sim_IPC | grep -o -E "[0-9]+\.[0-9]+"
-								echo -n -e "$config\t\tEnergy\t"
-								cat /tmp/stats_$path | grep "# total power per cycle_cc1" | grep -o -E "[0-9]+\.[0-9]+"
+								echo -n -e "\n$config\t"
 								
+								IPC=$( cat  /tmp/stats_$path | grep sim_IPC | grep -o -E "[0-9]+\.[0-9]+" )
+								Energy=$(cat /tmp/stats_$path | grep "# total power per cycle_cc1" | grep -o -E "[0-9]+\.[0-9]+")
+								
+								echo -e $IPC'\t'$Energy
 								
 			
 			
@@ -22,4 +23,5 @@ for bpred in nottaken taken perfect bimod 2lev comb; do
 		done
 	done
 done
+
 
