5eeaeeb8c1c9935421691923531361d84a694a18
Mattin Mir-Tahmasebi <mm5213@matrix43.doc.ic.ac.uk>
2017-02-16 17:36:27 +0000

    ras

--- varyorder.sh
+++ varyorder.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 
-stats="sim_IPC	total_power_cycle_cc1	sim_cycle	sim_elapsed_time"
+stats="total_power_cycle_cc1	sim_IPC	sim_cycle	sim_elapsed_time"
 
 
 
-echo -e "set\tassoc\t$stats"
+echo -e "order\t$stats"
 
 
-for set in 1 2 4 8 16 32 64; do 
-
+for c in true false ; do 
+	
 			
-			export SSFLAGS="-ruu:size 16 -bpred comb -bpred:btb $set 1"
+			export SSFLAGS="-ruu:size 16 -bpred comb -bpred:comb 256 -bpred:btb 32 1 -issue:inorder $c"
 			
-			config=$set
-			path=$set
-
-
-
+			config=$c
+			path=$c
+			
 			./run-wattch  &>  /tmp/stats_$path
 			
 			echo -n -e "$config\t"
 			
 			
 				
-			echo -n -e $( cat /tmp/stats_$path | grep "instructions per cycle" | grep -o -E " [0-9]+(\.[0-9]+)?" )'\t'
+			
 			echo -n -e $( cat /tmp/stats_$path | grep "total power per cycle_cc1" | grep -o -E " [0-9]+(\.[0-9]+)?" )'\t'
+			echo -n -e $( cat /tmp/stats_$path | grep "instructions per cycle" | grep -o -E " [0-9]+(\.[0-9]+)?" )'\t'
 			echo -n -e $( cat /tmp/stats_$path | grep "total simulation time in cycles" | grep -o -E " [0-9]+(\.[0-9]+)?" )'\t'
 			echo -n -e $( cat /tmp/stats_$path | grep "total simulation time in seconds" | grep -o -E " [0-9]+(\.[0-9]+)?" )'\t'
 			
 			echo ""
-
 done
 
 
